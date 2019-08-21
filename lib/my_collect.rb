56f97a41ffdccb1b2e8377d866924436e2884a7d
Steven Laff <steve.laff@gmail.com>
2019-08-21 00:26:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,23 +1,10 @@
-#def my_collect(languages)
-#  if !block_given?
-#    "Block not given."
-#  else
-#    colelction = []
-#    i = 0
-#    while i < languages.length do
-#      collection << yield(languages[i])
-#      i += 1
-#    end
-#  end
-#  collection
-#end
 
 def my_collect(array)
   i = 0
-  name_collection = []
+  collection = []
   while i < array.length
-    name_collection.push yield(array[i])
+    collection<< yield(array[i])
     i += 1
   end
-  name_collection
+  collection
 end
