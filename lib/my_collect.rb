48bd399aee5563cd06379330185cafdde6282470
Steven Laff <steve.laff@gmail.com>
2019-08-21 00:14:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,12 @@
 def my_collect(array)
+  if !block_given?
+    "Block not given."
+  else
     i = -1
     while i < array.length
     i += 1
       puts array[i] 
       yield(array[i])
     end
+  end
 end
